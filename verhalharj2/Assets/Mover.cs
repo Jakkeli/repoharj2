e2df00ea8ba1d621a1992cbaf35e9c7bf25ae32c
Jaakko Haavisto <jaako.haavisto@kolumbus.fi>
2017-04-20 13:45:48 +0300

    first thing done. it's an amazing capsule!!11!!

---
+++ verhalharj2/Assets/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+        transform.Rotate(0, 1, 0);
+        transform.Translate(0.1f, 0, 0);
+	}
+}
